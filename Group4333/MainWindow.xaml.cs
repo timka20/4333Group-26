6a816fd3fbca76cb69661ec70214c856954c856d
timka20 <mta200307@yandex.ru>
2026-02-17 17:30:45 +0300

    Add my info to 4333Group-26

--- Group4333/MainWindow.xaml.cs
+++ Group4333/MainWindow.xaml.cs
@@ -6,5 +6,12 @@ namespace Group4333
     {
         public MainWindow()
             => InitializeComponent();
+
+        private void AuthorButton_Click(object sender, RoutedEventArgs e)
+        {
+            var infoWindow = new _4333_Minibaev();
+            infoWindow.Owner = this;
+            infoWindow.ShowDialog();
+        }
     }
 }
